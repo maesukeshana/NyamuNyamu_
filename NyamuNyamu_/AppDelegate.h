f45617a99a2963d442bea94c81fa3f1581bdbb67
Yusuke Maenishi <Maesuke@Yusuke-no-MacBook-Pro.local>
2013-02-11 13:06:09 +0900

    Initial Commit

---
+++ NyamuNyamu_/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NyamuNyamu_
+//
+//  Created by Yusuke Maenishi on 2013/02/11.
+//  Copyright (c) 2013年 Maesuke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
